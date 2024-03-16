a0edd8d8853c1a11ea73a733a5dd43161d601808
Liman Wu <limanwu16@gmail.com>
2024-03-16 21:26:16 +1300

    css for MyListingPage

--- src/Pages/MyListingsPage.js
+++ src/Pages/MyListingsPage.js
@@ -11,49 +11,57 @@ export default function MyListingsPage() {
     const [loading, setLoading] = useState(true); // State to track loading status
     const auth = getAuth();
 
+    //########################################
+
+    //need to create indexes that need to be used to power compound queries in firestore
     useEffect(() => {
         async function fetchUserListings() {
-            try {
-                setLoading(true);
-                const listingRef = collection(db, "listings");
-                const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp", "desc"));
-                const querySnapshot = await getDocs(q);
-                const listingsArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                setListings(listingsArray); // Update the listings state with the fetched data
-            } catch (error) {
-                console.error("Error fetching listings: ", error);
-                // Handle the error appropriately in your application
-            } finally {
-                setLoading(false); // Ensure loading is set to false when operation is complete
-            }
+        const listingRef = collection(db, "listings");
+        const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp", "desc"));
+        const querySnapshot = await getDocs(q);
+        let listings = [];
+        querySnapshot.forEach((doc) => {
+            return listings.push({
+            id:doc.id,
+            data:doc.data(),
+            });
+        });
+        setListings(listings); // Update the listings state with the fetched data
+        setLoading(false);
         }
         fetchUserListings();
-    }, [auth.currentUser?.uid]); // Dependency array includes auth.currentUser.uid to refetch when it changes
+    }, [auth.currentUser.uid]); // Dependency array includes auth.currentUser.uid to refetch when it changes
+
+
 
     return (
         <div>
             <Header />
-            <div className="max-w-6xl px-3 mt-6 mx-auto">
-                {!loading && listings.length > 0 && (
-                <>
-                    <h2 className="text-2xl text-center font-semibold mb-6">
-                    My Listings
-                    </h2>
-                    <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-                    {listings.map((listing) => (
-                        //create a component for ListingItem
-                        <ListingItem
-                        key={listing.id}
-                        id={listing.id}
-                        listing={listing.data}
-                        // onDelete={() => onDelete(listing.id)}
-                        // onEdit={() => onEdit(listing.id)}
-                        />
-                    ))}
-                    </ul>
-                </>
-                )}
-            </div>            
+
+            {/* ##################################################### */}
+            <div>
+                <div className="max-w-6xl px-3 mt-10 mx-auto">
+                    {!loading && listings.length > 0 && (
+                    <>
+                        <h2 className="text-center font-semibold mb-10 text-2xl text-sky-800">
+                        My Listings
+                        </h2>
+                        <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+                        {listings.map((listing) => (
+                            //create a component for ListingItem
+                            <ListingItem
+                            key={listing.id}
+                            id={listing.id}
+                            listing={listing.data}
+                            // onDelete={() => onDelete(listing.id)}
+                            // onEdit={() => onEdit(listing.id)}
+                            />
+                        ))}
+                        </ul>
+                    </>
+                    )}
+                </div>            
+            </div>           
         </div>
     );
 }
--- src/Pages/UserProfilePage.js
+++ src/Pages/UserProfilePage.js
@@ -13,14 +13,14 @@ import { db } from "../firebase";     // import db object for configuration and 
 
 import profileSideImage from "../assets/profileSideImage.jpg";
 
-import { collection, query, orderBy, where, getDocs } from 'firebase/firestore';
-import ListingItem from '../Components/ListingItem';
+//import { collection, query, orderBy, where, getDocs } from 'firebase/firestore';
+//import ListingItem from '../Components/ListingItem';
 
 
 export default function UserProfilePage() {
   const auth = getAuth();
-  const [listings, setListings] = useState([]); // State to store the listings
-  const [loading, setLoading] = useState(true); // State to track loading status
+  //const [listings, setListings] = useState([]); // State to store the listings
+  //const [loading, setLoading] = useState(true); // State to track loading status
 
   //fetch data for users collection
     const [userInfo, setUserInfo] = useState({});
@@ -94,28 +94,7 @@ export default function UserProfilePage() {
     }
   }
 
-  //########################################
-
-  //need to create indexes that need to be used to power compound queries in firestore
-  useEffect(() => {
-      async function fetchUserListings() {
-        const listingRef = collection(db, "listings");
-        const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp", "desc"));
-        const querySnapshot = await getDocs(q);
-        let listings = [];
-        querySnapshot.forEach((doc) => {
-          return listings.push({
-            id:doc.id,
-            data:doc.data(),
-          });
-        });
-        setListings(listings); // Update the listings state with the fetched data
-        setLoading(false);
-      }
-      fetchUserListings();
-  }, [auth.currentUser.uid]); // Dependency array includes auth.currentUser.uid to refetch when it changes
-
-
+  
   return (
     <div>
       <Header/>
@@ -241,34 +220,6 @@ export default function UserProfilePage() {
 
         </section>
       </div>
-
-
-      {/* ##################################################### */}
-      <div>
-          <div className="max-w-6xl px-3 mt-10 mx-auto">
-              {!loading && listings.length > 0 && (
-              <>
-                  <h2 className="text-center font-semibold mb-10 text-2xl text-sky-800">
-                  My Listings
-                  </h2>
-                  <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-                  {listings.map((listing) => (
-                      //create a component for ListingItem
-                      <ListingItem
-                      key={listing.id}
-                      id={listing.id}
-                      listing={listing.data}
-                      // onDelete={() => onDelete(listing.id)}
-                      // onEdit={() => onEdit(listing.id)}
-                      />
-                  ))}
-                  </ul>
-              </>
-              )}
-          </div>            
-      </div>
-       
-
     </div>
 
         
